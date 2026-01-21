0e3d58dc15ad1a9f183f40894447b1a23cd261f1
Natanim Issa <69296809+Natanim-I@users.noreply.github.com>
2026-01-21 17:22:56 +0000

    first day docker and pipeline workshop

---
+++ pipeline/pipeline.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+
+# print(f"arguments: {sys.argv}")
+day = sys.argv[1]
+print(f"Hello Pipeline, Running pipeline for day:- {day}")
+
+df = pd.DataFrame({"A": [1,2], "B": [3,4]})
+df["day"] = day
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
