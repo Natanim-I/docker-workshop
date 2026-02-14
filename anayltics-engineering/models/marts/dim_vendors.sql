5d521eb8e4a25b70cf4501a43ecaae6bcd42ffdd
Natanim Issa <natanimissa@gmail.com>
2026-02-14 21:49:25 +0000

    created first macro function

--- anayltics-engineering/models/marts/dim_vendors.sql
+++ anayltics-engineering/models/marts/dim_vendors.sql
@@ -1,1 +1,17 @@
-select 1
+with trips_unioned as (
+    select * from {{ref('int_trips_unions')}}
+),
+
+vendors as (
+    select 
+        distinct vendor_id,
+        case 
+            when vendor_id = 1 then 'Creative Mobile Technologies, LLC'
+            when vendor_id = 2 then 'VeriFone Inc'
+            when vendor_id = 4 then 'Unknown Vendors'
+        end as vendor_name
+
+    from trips_unioned
+)
+
+select * from vendors
