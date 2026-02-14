105913fa0c23fdd5b8685a5ac247d64272e2f54f
Natanim Issa <natanimissa@gmail.com>
2026-02-14 22:20:03 +0000

    fixed bug on get_vendors_name macro

--- anayltics-engineering/macros/get_vendor_names.sql
+++ anayltics-engineering/macros/get_vendor_names.sql
@@ -3,5 +3,5 @@ case
     when {{vendor_id}} = 1 then 'Creative Mobile Technologies, LLC'
     when {{vendor_id}} = 2 then 'VeriFone Inc.'
     when {{vendor_id}} = 4 then 'Unknown Vendors'
-end as vendor_name
+end 
 {%- endmacro %}
--- anayltics-engineering/models/marts/dim_vendors.sql
+++ anayltics-engineering/models/marts/dim_vendors.sql
@@ -6,7 +6,6 @@ vendors as (
     select 
         distinct vendor_id,
         {{ get_vendor_names('vendor_id') }} as vendor_name
-
     from trips_unioned
 )
 
